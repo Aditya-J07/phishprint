423d4e9f242368503a980b59f6099ea9ba8844b8
Aditya Jha <adityajha.vats02@gmail.com>
2025-12-12 20:01:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -804,5 +804,5 @@ def main():
     app = PhishPrintApp()
     app.run()
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
